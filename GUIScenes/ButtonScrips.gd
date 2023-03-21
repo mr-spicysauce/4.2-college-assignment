bfc2f2f1da342bdb49d111e3284bfee32069aaf1
mr_spicysauce <105018319+mr-spicysauce@users.noreply.github.com>
2023-03-21 19:35:14 +0000

    MainMenuTestCommit

---
+++ GUIScenes/ButtonScrips.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+func _on_StartButton_pressed():
+	pass # Replace with function body.
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
---
+++ Scripts/GithubLikeButton.gd
@@ -0,0 +1,5 @@
+extends LinkButton
+
+
+func _on_GitHubLikeButton_pressed():
+	OS.shell_open("https://github.com/mr-spicysauce/4.2-college-assignment")
